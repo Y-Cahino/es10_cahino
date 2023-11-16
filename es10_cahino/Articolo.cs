330e7a862aed4b477743d06fea5052b0ca04fb5c
Cahinosilva.20184 <Cahinosilva.20184@E2-9-PC11>
2023-11-16 10:19:18 +0100

    Aggiungere i file di progetto.

---
+++ es10_cahino/Articolo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace es10_cahino
+{
+
+    class Articolo
+    {
+        protected int code;
+        protected string n;
+        protected double pr;
+        //Costruttore
+        public Articolo(int code, string n, double pr)
+        {
+            this.code = code;
+            this.n = n;
+            this.pr = pr;
+        }
+        public void sconta()
+        {
+
+        }
+    }
+}
